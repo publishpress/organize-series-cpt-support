808aebf13810cd7c000e4412cc05fdf93ad7ebcc
Darren Ethier <darren@roughsmootheng.in>
2017-10-02 20:44:28 -0400

    implement support for new licensing/bootstrap service in os core #4

---
+++ src/domain/Meta.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace OrganizeSeries\CptSupportAddon\domain;
+
+use OrganizeSeries\domain\interfaces\AbstractMeta;
+
+class Meta extends AbstractMeta
+{
+    //Used for any multiples specific paths etc.
+}
